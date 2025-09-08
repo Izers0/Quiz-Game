7b7ccfe19574bd8e98c8569b1c0d60db6bce28a9
Isaac <isaacatkinson04@gmail.com>
2025-09-08 03:41:28 +0100

    Array to hold questions 2d array to hold answers for multiple choice answers

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main() {
 
+    string questions [] = {"1. How was luffy first introduced?",
+                            "2. What is Sanjis dream?",
+                            "3. Where did luffy first meet Nami?",
+                            "4. What is Usopp most known for?",
+                            "5. What is the name of the sword Zoro first recieved?"};
 
-
+    string answers [][4] = { // question 1 answers
+                            {"In a barrel lost at sea", "Imprisoned by an enemy", "With Shanks",
+                            "Training to be a marine" },
+                             // question 2 answers
+                            {"To cook the best meal in the world", "To own his own restaurant",
+                                "To find the All-Blue", "To be Stronger than Zoro"},
+                             // question 3 answers
+                                {"Arlong Park", "On his way to Orange Town", "The Grandline",
+                                    "Alabasta"},
+                             // question 4 answers
+                             {"Being a good captain", "His inventions", "Art", "Lying"},
+                             // question 5 answers
+                                {"Enma", "Wado-Ichimoji", " Sandai Kitetsu", "Shusui"}
+                            };
 }
