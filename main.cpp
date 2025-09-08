ef8906da618e3076efdb37c3057143f977a08f75
Isaac <isaacatkinson04@gmail.com>
2025-09-08 05:18:58 +0100

    Changed askQuestion function to only have 3 parameters being: string questions[], string answers[][4], char correctAnswers[]
    This allows the program to handle one question at a time in the function and later the main function will loop through each question

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool askQuestion (string questions[], string answers[], char correctAnswers[], int questionSize, int answerRows,
-                    int answerCols, int correctAnswerSize);
+bool askQuestion (string questions[], string answers[][4], char correctAnswers[]);
 
 int main() {
 
@@ -13,52 +12,41 @@ int main() {
                             "4. What is Usopp most known for?",
                             "5. What is the name of the sword Zoro first recieved?"};
 
-    // size of questions array
+    /* size of questions array
     int questionSize = sizeof(questions) / sizeof(questions[0]);
+    */
 
     // Array of answers
     string answers [][4] = { // question 1 answers - A
                             {"In a barrel lost at sea", "Imprisoned by an enemy", "With Shanks",
-                            "Training to be a marine" },
+                                "Training to be a marine" },
                              // question 2 answers - C
                             {"To cook the best meal in the world", "To own his own restaurant",
                                 "To find the All-Blue", "To be Stronger than Zoro"},
                              // question 3 answers - B
-                                {"Arlong Park", "On his way to Orange Town", "The Grandline",
-                                    "Alabasta"},
+                            {"Arlong Park", "On his way to Orange Town", "The Grandline",
+                                "Alabasta"},
                              // question 4 answers - D
-                             {"Being a good captain", "His inventions", "Art", "Lying"},
+                            {"Being a good captain", "His inventions", "Art", "Lying"},
                              // question 5 answers - B
-                                {"Enma", "Wado-Ichimoji", " Sandai Kitetsu", "Shusui"}
+                            {"Enma", "Wado-Ichimoji", " Sandai Kitetsu", "Shusui"}
                             };
 
-    // size of answers array
+    /* size of answers array
     int answerRows = sizeof(answers) / sizeof(answers[0]);
     int answerCols = sizeof(answers) / sizeof(answers[0]);
+    */
 
     // Array to hold the correct answers to check against user input
     char correctAnswers[] = {'A', 'C', 'B', 'D', 'B'};
 
-    // size of correctAnswer array
+    /* size of correctAnswer array
     int correctAnswerSize = sizeof(correctAnswers) / sizeof(correctAnswers[0]);
+    */
 
-    askQuestion(questions, *answers, correctAnswers, questionSize, answerRows, answerCols, correctAnswerSize);
+    askQuestion(questions, answers, correctAnswers);
 }
 
-bool askQuestion (string questions[], string answers[][4], char correctAnswers[], int questionSize,int answerRows,
-                    int answerCols, int correctAnswerSize) {
-
-    // display questions
-    for (int i = 0; i < questionSize; i++) {
-        cout << questions[i];
-    }
-
-    // display answers for the question
-    for (int i = 0; i < answerRows; i++) {
-        for (int j = 0; j < answerCols; j++) {
-            cout << answers[i][j] << " ";
-        }
-    }
-
+bool askQuestion (string questions[], string answers[][4], char correctAnswers[]) {
     return true;
 }
