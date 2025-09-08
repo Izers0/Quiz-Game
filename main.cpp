df8e7c7ef77e489fb73c467d09dead7e5fcfbb2c
Isaac <isaacatkinson04@gmail.com>
2025-09-08 04:59:07 +0100

    Added array to hold the correct answer of each question Function to handle the questions and answers

--- main.cpp
+++ main.cpp
@@ -1,26 +1,64 @@
 #include <iostream>
 using namespace std;
 
+bool askQuestion (string questions[], string answers[], char correctAnswers[], int questionSize, int answerRows,
+                    int answerCols, int correctAnswerSize);
+
 int main() {
 
+    // Array of questions
     string questions [] = {"1. How was luffy first introduced?",
                             "2. What is Sanjis dream?",
                             "3. Where did luffy first meet Nami?",
                             "4. What is Usopp most known for?",
                             "5. What is the name of the sword Zoro first recieved?"};
 
-    string answers [][4] = { // question 1 answers
+    // size of questions array
+    int questionSize = sizeof(questions) / sizeof(questions[0]);
+
+    // Array of answers
+    string answers [][4] = { // question 1 answers - A
                             {"In a barrel lost at sea", "Imprisoned by an enemy", "With Shanks",
                             "Training to be a marine" },
-                             // question 2 answers
+                             // question 2 answers - C
                             {"To cook the best meal in the world", "To own his own restaurant",
                                 "To find the All-Blue", "To be Stronger than Zoro"},
-                             // question 3 answers
+                             // question 3 answers - B
                                 {"Arlong Park", "On his way to Orange Town", "The Grandline",
                                     "Alabasta"},
-                             // question 4 answers
+                             // question 4 answers - D
                              {"Being a good captain", "His inventions", "Art", "Lying"},
-                             // question 5 answers
+                             // question 5 answers - B
                                 {"Enma", "Wado-Ichimoji", " Sandai Kitetsu", "Shusui"}
                             };
+
+    // size of answers array
+    int answerRows = sizeof(answers) / sizeof(answers[0]);
+    int answerCols = sizeof(answers) / sizeof(answers[0]);
+
+    // Array to hold the correct answers to check against user input
+    char correctAnswers[] = {'A', 'C', 'B', 'D', 'B'};
+
+    // size of correctAnswer array
+    int correctAnswerSize = sizeof(correctAnswers) / sizeof(correctAnswers[0]);
+
+    askQuestion(questions, *answers, correctAnswers, questionSize, answerRows, answerCols, correctAnswerSize);
+}
+
+bool askQuestion (string questions[], string answers[][4], char correctAnswers[], int questionSize,int answerRows,
+                    int answerCols, int correctAnswerSize) {
+
+    // display questions
+    for (int i = 0; i < questionSize; i++) {
+        cout << questions[i];
+    }
+
+    // display answers for the question
+    for (int i = 0; i < answerRows; i++) {
+        for (int j = 0; j < answerCols; j++) {
+            cout << answers[i][j] << " ";
+        }
+    }
+
+    return true;
 }
